07f9f78895a8ccb18ad17e372fa908be52c712e9
marnixah <marnix@hagehoeve.com>
2022-03-14 12:41:16 +0100

    feat: light control

---
+++ src/Classes/Utils/HomeAssistant.ts
@@ -0,0 +1,30 @@
+import assert from "assert";
+import fetch from "node-fetch";
+
+export default class HomeAssistant {
+  private url = "https://homeassistant.marnixah.com/";
+  private token: string;
+
+  constructor() {
+    assert(process.env.HA_TOKEN, "HomeAssistant token is required");
+    this.token = process.env.HA_TOKEN;
+  }
+
+  public call(endpoint: string, entity: string) {
+    const url = `${this.url}${endpoint}`;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${this.token}`,
+    };
+    const body = JSON.stringify({ entity_id: entity });
+    return fetch(url, { method: "POST", headers, body });
+  }
+
+  public turnOn(entity: string) {
+    return this.call("api/services/light/turn_on", entity);
+  }
+
+  public turnOff(entity: string) {
+    return this.call("api/services/light/turn_off", entity);
+  }
+}
---
+++ src/Commands/Lights.ts
@@ -0,0 +1,39 @@
+import BaseMessageContext from "src/Classes/Contexts/BaseMessageContext";
+import BaseCommand from "./BaseCommand";
+import HomeAssistant from "../Classes/Utils/HomeAssistant";
+
+export default class LightsCommand implements BaseCommand {
+  public name = "lights";
+  public aliases = ["turnon", "turnoff", "turn"];
+
+  private entity = "light.marnix_bulb";
+  private hass = new HomeAssistant();
+
+  public async run(ctx: BaseMessageContext) {
+    const words = ctx.message.split(" ").map((word) => word.toLowerCase());
+
+    // Can be either on or off
+    const targetState = words.includes("on")
+      ? "on"
+      : words.includes("off")
+      ? "off"
+      : null;
+
+    if (!targetState) {
+      ctx.reply("E-Error, I couldn't figure out the desired state");
+      return ctx.close();
+    }
+
+    const res =
+      targetState === "on"
+        ? await this.hass.turnOn(this.entity)
+        : await this.hass.turnOff(this.entity);
+
+    if (res.status === 200) {
+      ctx.reply(`I turned ${targetState} the light`);
+    } else {
+      ctx.reply(`E-Error, I couldn't turn the light ${targetState}`);
+    }
+    return ctx.close();
+  }
+}
