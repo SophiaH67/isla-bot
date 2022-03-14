b429ed3f36dfd29ffe3721dbb3c1eb01edc7ef39
marnixah <marnix@hagehoeve.com>
2022-03-14 10:13:57 +0100

    feat(TimeParser): more valid times

--- src/Classes/Utils/TimeParser.ts
+++ src/Classes/Utils/TimeParser.ts
@@ -1,6 +1,23 @@
 export default class TimeParser {
   public text = "";
   public time: number | undefined;
+  private multiplierDict = {
+    s: 1,
+    sec: 1,
+    secs: 1,
+    seconds: 1,
+    m: 60,
+    min: 60,
+    mins: 60,
+    minutes: 60,
+    h: 60 * 60,
+    hr: 60 * 60,
+    hrs: 60 * 60,
+    hours: 60 * 60,
+    d: 60 * 60 * 24,
+    day: 60 * 60 * 24,
+    days: 60 * 60 * 24,
+  };
 
   constructor(text: string) {
     this.text = text;
@@ -8,20 +25,14 @@ export default class TimeParser {
   }
 
   private parse() {
-    const time = this.text.match(/(\d+)\s*(mins|hours|days)/);
-    if (time) {
-      this.time = parseInt(time[1], 10);
-      switch (time[2]) {
-        case "mins":
-          this.time *= 60;
-          break;
-        case "hours":
-          this.time *= 60 * 60;
-          break;
-        case "days":
-          this.time *= 60 * 60 * 24;
-          break;
-      }
+    const time = this.text.match(
+      /(\d+)\s*(s|sec|secs|seconds|m|min|mins|minutes|h|hr|hrs|hours|d|day|days)/
+    );
+    if (!time || !(time[2] in this.multiplierDict)) {
+      return;
     }
+
+    // @ts-ignore because we know it's a number
+    this.time = parseInt(time[1], 10) * this.multiplierDict[time[2]];
   }
 }
