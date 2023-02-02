4c33691999b9b66a80428082721f5d9716081740
marnixah <marnix@roboco.dev>
2023-02-02 14:10:11 +0100

    fix: :bug: I love TypeScript very very much!!!!

--- src/Classes/Frontends/BaseFrontend.ts
+++ src/Classes/Frontends/BaseFrontend.ts
@@ -1,7 +1,7 @@
 import Protocol from "../protocol/Protocol";
 
 export default abstract class BaseFrontend {
-  public async broadcast(message: string): Promise<void> {}
+  public async broadcast(_message: string): Promise<void> {}
   public async start(): Promise<void> {}
   public async setProtocol(_protocol: Protocol): Promise<void> {}
 }
