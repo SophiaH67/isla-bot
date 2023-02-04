1b48fbdce26a957c786f02f92c6b31fc1f9e3c91
marnixah <marnix@roboco.dev>
2023-02-04 18:13:53 +0100

    feat: :sparkles: allow overriding of protocol check

--- src/Commands/ProtocolCommand.ts
+++ src/Commands/ProtocolCommand.ts
@@ -6,7 +6,7 @@ import Protocol from "../Classes/protocol/Protocol";
 export default class EchoCommand implements Command {
   public aliases = ["protocol", "emergency"];
   public description = "Changes current safety protocol";
-  public usage = "protocol <protocol>";
+  public usage = "protocol <protocol> [override]";
 
   public async run(
     conversation: Conversation,
@@ -24,7 +24,9 @@ export default class EchoCommand implements Command {
 
     const isla = conversation.eris as Isla;
 
-    if (targetProtocol === isla.protocolManager.protocol)
+    const override = args.join(" ").toLowerCase().includes("override");
+
+    if (targetProtocol === isla.protocolManager.protocol && !override)
       return `Protocol is already set to '${targetProtocol}'`;
 
     isla.protocolManager.setProtocol(targetProtocol);
