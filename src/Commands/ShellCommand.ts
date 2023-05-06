eda152af35f67769a191325a44268d82c389670a
marnixah <marnix@roboco.dev>
2023-05-07 00:10:04 +0200

    fix ooo exit code mixed in with stderr/stdout

--- src/Commands/ShellCommand.ts
+++ src/Commands/ShellCommand.ts
@@ -2,10 +2,58 @@ import Command from "eris-boreas/lib/src/conversation/Command";
 import Conversation from "eris-boreas/lib/src/conversation/Conversation";
 import cp from "child_process";
 
+/**
+ * DataSpooler is a class that data can be written to so that it comes
+ * out in the order it was written. This is useful, since, for example,
+ * with the shell command, if a lot of data is written to stdout, the
+ * process can exit before all of the data is written to the channel.
+ * This means that the status code is written in the middle of the data
+ * that was written to stdout, which is not what we want.
+ */
+class DataSpooler {
+  private data: string[] = [];
+  private _isSpooling = false;
+  private afterSpoolingCallback: ((value?: unknown) => void) | null = null;
+
+  public get isSpooling() {
+    return this._isSpooling;
+  }
+
+  constructor(private writeToChannel: (data: string) => Promise<unknown>) {}
+
+  private async spool() {
+    this._isSpooling = true;
+    while (this.data.length > 0) {
+      const data = this.data.shift();
+      if (!data?.trim()) continue;
+      await this.writeToChannel(data);
+    }
+    this._isSpooling = false;
+    if (this.afterSpoolingCallback) this.afterSpoolingCallback();
+  }
+
+  public write(data: string) {
+    this.data.push(data);
+    if (!this._isSpooling) {
+      this.spool();
+    }
+  }
+
+  public flush() {
+    return new Promise((resolve) => {
+      if (!this._isSpooling) {
+        resolve(undefined);
+        return;
+      }
+      this.afterSpoolingCallback = resolve;
+    });
+  }
+}
+
 export default class ShellCommand implements Command {
   public description = "Execute a shell command";
   public usage = "shell <command>";
-  public aliases = ["shell", "sh", "!", "eval"];
+  public aliases = ["shell ", "sh ", "!", "eval "];
 
   private allowedUsers = ["123", "178210163369574401", "231446107794702336"];
 
@@ -21,17 +69,14 @@ export default class ShellCommand implements Command {
       stdio: ["ignore", "pipe", "pipe"],
     });
 
-    const handleData = (data: string) => {
-      data = data.trim();
-      if (!data) return;
-      conversation.write(data);
-    };
+    const spooler = new DataSpooler((data) => conversation.write(data));
 
-    child.stdout.on("data", (data) => handleData(data.toString()));
-    child.stderr.on("data", (data) => handleData(data.toString()));
+    child.stdout.on("data", (data) => spooler.write(data.toString()));
+    child.stderr.on("data", (data) => spooler.write(data.toString()));
 
     return new Promise((resolve) => {
-      child.on("close", (code) => {
+      child.on("close", async (code) => {
+        await spooler.flush();
         resolve(`Process exited with code ${code}`);
       });
     });
