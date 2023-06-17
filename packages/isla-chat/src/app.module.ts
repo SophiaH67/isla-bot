59757421cff0d720630ba585b42c85fee9e5c477
marnixah <marnix@roboco.dev>
2023-06-17 20:34:24 +0200

    load .env stuff

--- packages/isla-chat/src/app.module.ts
+++ packages/isla-chat/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IslaChatModule } from './isla-chat/isla-chat.module';
 import { DirectiveHandlerModule } from './directive-handler/directive-handler.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [IslaChatModule, DirectiveHandlerModule],
+  imports: [IslaChatModule, DirectiveHandlerModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
