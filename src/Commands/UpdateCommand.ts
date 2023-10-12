3b319cf4a195c0d9d7bff52ed13a5a631c70b099
Sophia Hage <sophia@sophiah.gay>
2023-10-12 19:08:50 +0200

    Unlock restart manager on update

--- src/Commands/UpdateCommand.ts
+++ src/Commands/UpdateCommand.ts
@@ -3,6 +3,7 @@ import Conversation from "../Classes/Utils/Conversation";
 import { AdminGuard } from "../Classes/Utils/AdminGuard";
 import LoggingService from "../Services/LoggingService";
 import { t } from "../Classes/mood/dicts";
+import UnexpectedRestartService from "../Services/UnexpectedRestartService";
 
 export default class UpdateCommand implements Command {
   public aliases = ["updateisla", "upgradeisla"];
@@ -18,10 +19,16 @@ export default class UpdateCommand implements Command {
       .getService(LoggingService)
       .getLogger(UpdateCommand.name);
 
+    const unexpectedRestartService = conversation.isla.getService(
+      UnexpectedRestartService
+    );
+
     // Spawn an alpine docker container to update this instance of Isla
     const { exec } = require("child_process");
 
     logger.warn(t(conversation, "updateStarting"));
+
+    await unexpectedRestartService.unlock();
 
     exec(
       'docker run -d --rm -v /var/run/docker.sock:/var/run/docker.sock -v /Fuwawa/config/docker-compose/isla:/Fuwawa/config/docker-compose/isla docker sh -c "cd /Fuwawa/config/docker-compose/isla && docker compose pull && docker compose up -d"',
