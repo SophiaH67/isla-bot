1bfeb018b0855e8701a0cd02395d023f572992b7
Sophia Hage <sophia@sophiah.gay>
2023-10-12 20:10:20 +0200

    Move event args to helper types

--- src/Services/BaseService.ts
+++ src/Services/BaseService.ts
@@ -1,10 +1,20 @@
 import { IslaMessage } from "../Classes/interfaces/IslaMessage";
 import { Protocol } from "./ProtocolService";
 
-export interface BaseService {
-  onMessage?(message: IslaMessage): Promise<void>;
-  onMessageUpdate?(message: IslaMessage): Promise<void>;
-  onProtocolChange?(protocol: Protocol): Promise<void>;
+type Events = {
+  message: [IslaMessage]; // message
+  messageUpdate: [IslaMessage]; // message
+  protocolChange: [Protocol]; // protocol
+  start: []; // void
+};
+
+type EventListeners = {
+  [K in keyof Events as `on${Capitalize<string & K>}`]: (
+    ...args: Events[K]
+  ) => Promise<void> | void;
+};
+
+export interface BaseService extends Partial<EventListeners> {
   start?(): Promise<void>;
 
   // Make typescript not complain that there is nothing in common
